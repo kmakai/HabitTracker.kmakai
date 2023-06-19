b7c4e95fe54b4dca812064f6d8742f6d4772081a
kmakai <makaikwag@gmail.com>
2023-06-19 17:15:27 -0400

    working on menu

---
+++ HabitTracker.kmakai/HabitTracker.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitTracker.kmakai;
+
+public class HabitTracker
+{
+    private List<string> _habitsList { get; set; } = new List<string>();
+    private string? _connectionString { get; set; } = @"Data Source=Habit_Tracker.db";
+    
+    public void start()
+    {
+        while (true)
+        {
+            TrackerMenu.MainMenu();
+            string option = TrackerMenu.GetOption();
+        }
+    }
+    public void AddHabit()
+    {
+        string habit = TrackerMenu.GetOption("Please enter the habit you want to add use underscore ( _ ) for spaces: ");
+        string UnitOfMeasurement = TrackerMenu.GetOption("Please enter the unit of measurement all will: ");
+
+
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            var tableCommand = connection.CreateCommand();
+
+            tableCommand.CommandText = @$"CREATE TABLE IF NOT EXISTS {habit} (
+                                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Date TEXT,
+                                            {UnitOfMeasurement} Double,
+                                        )";
+
+            tableCommand.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        _habitsList.Add(habit);
+    }
+}
--- HabitTracker.kmakai/TrackerMenu.cs
+++ HabitTracker.kmakai/TrackerMenu.cs
@@ -1,5 +1,43 @@
 ﻿namespace HabitTracker.kmakai;
 
 public class TrackerMenu
-{ 
+{
+    public static void MainMenu()
+    {
+        Console.WriteLine("Welcome to the Habit Tracker!");
+        Console.WriteLine("Please select an option:");
+        Console.WriteLine("1. Add a habit");
+        Console.WriteLine("2. Remove a habit");
+        Console.WriteLine("3. Manage habits");
+        Console.WriteLine("0. Exit");
+    }
+
+    public static void ManageHabitsMenu(List<string> list)
+    {
+        Console.WriteLine("Please select the habit:");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {list[i]}");
+        }
+
+    }
+
+    public static string GetOption(string? message = null)
+    {
+        if (message != null) Console.WriteLine(message);
+        Console.Write("\nEnter your input: ");
+
+        string? option = Console.ReadLine();
+
+        while (option == null || option == "")
+        {
+            Console.WriteLine("Please enter a valid input!");
+            Console.Write("\nEnter your input: ");
+            option = Console.ReadLine();
+        }
+
+        return option;
+    }
 }
+
+
